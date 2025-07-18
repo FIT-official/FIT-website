92e60d5a8d94c8174cca631b118b8419ce6502fe
saboiii <saba.x.azad@gmail.com>
2025-07-18 21:31:30 +0800

    wrong place to put suspense

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,18 +23,20 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={`${inter.variable} antialiased`}>
           <Smooth>
-            <ToastProvider>
-              <div className="flex flex-row items-center justify-center bg-baseColor">
-                <div className="flex flex-col md:w-[90vw] lg:w-[85vw] max-w-[1350px] w-screen border-l border-r border-borderColor transition-all duration-300 ease-in-out overflow-hidden bg-background">
-                  <Suspense>
+            <Suspense>
+              <ToastProvider>
+                <div className="flex flex-row items-center justify-center bg-baseColor">
+                  <div className="flex flex-col md:w-[90vw] lg:w-[85vw] max-w-[1350px] w-screen border-l border-r border-borderColor transition-all duration-300 ease-in-out overflow-hidden bg-background">
+
                     <Navbar />
-                  </Suspense>
-                  <div className='lg:hidden flex h-16 w-full bg-background' />
-                  {children}
-                  <Footer />
+
+                    <div className='lg:hidden flex h-16 w-full bg-background' />
+                    {children}
+                    <Footer />
+                  </div>
                 </div>
-              </div>
-            </ToastProvider>
+              </ToastProvider>
+            </Suspense>
           </Smooth>
         </body>
       </html>
