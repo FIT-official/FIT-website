d5ad4063f019c879cd5015315f6756668116ed0e
saboiii <saba.x.azad@gmail.com>
2025-07-18 21:28:11 +0800

    suspense

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/General/Navbar";
 import Footer from "@/components/General/Footer";
 import Smooth from "@/components/General/Smooth";
 import { ToastProvider } from "@/components/General/ToastProvider";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,14 +20,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-
       <html lang="en">
         <body className={`${inter.variable} antialiased`}>
           <Smooth>
             <ToastProvider>
               <div className="flex flex-row items-center justify-center bg-baseColor">
                 <div className="flex flex-col md:w-[90vw] lg:w-[85vw] max-w-[1350px] w-screen border-l border-r border-borderColor transition-all duration-300 ease-in-out overflow-hidden bg-background">
-                  <Navbar />
+                  <Suspense>
+                    <Navbar />
+                  </Suspense>
                   <div className='lg:hidden flex h-16 w-full bg-background' />
                   {children}
                   <Footer />
